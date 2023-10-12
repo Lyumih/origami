102cf734c016bbb772f8128e5ffc23415b5bd346
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-12 23:26:53 +0300

    fix link

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,10 +1,10 @@
 namespace $.$$ {
 	export class $origami_app extends $.$origami_app {
-		
-		@ $mol_mem
+
+		@$mol_mem
 		todo_test() {
-			let result = this.$.$mol_fetch.json('http://31.129.109.90/todo/f9850879-d439-434d-88c2-a4797d90b110')
-			console.log(result)
+			let result = this.$.$mol_fetch.json( 'https://origami-team.site/todo/f9850879-d439-434d-88c2-a4797d90b110' )
+			console.log( result )
 			return result
 		}
 	}
