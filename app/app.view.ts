868514ec24211b4330d90254446b0b6f4f5ac05c
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-13 21:50:57 +0300

    fix autorun map

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,6 +1,11 @@
 namespace $.$$ {
 	export class $origami_app extends $.$origami_app {
 
+		autorun(): void {
+			$mol_map_yandex.api_key = () => "3b2caac0-490e-4fdc-92de-d2a09e400365"
+			super.autorun()
+		}
+
 		@$mol_mem
 		todo_test() {
 			let result = this.$.$mol_fetch.json( 'https://origami-team.site/todo/f9850879-d439-434d-88c2-a4797d90b110' )
--- map/map.view.ts
+++ map/map.view.ts
@@ -1,13 +1,5 @@
 namespace $.$$ {
 	export class $origami_map extends $.$origami_map {
-		
-		static api() {
-			console.log(123)
-			const KEY = '3b2caac0-490e-4fdc-92de-d2a09e400365'
-			let a = $mol_map_yandex.api()
-			console.log(a)
-			return $mol_import.script( `https://api-maps.yandex.ru/2.1/?lang=${ $mol_locale.lang() }` ).ymaps
-		}
-		
+
 	}
 }
