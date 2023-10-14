2a390ec982f0430e18445305e1cd99db6cc7593d
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-14 20:02:37 +0300

    ашч

--- app/world/world.view.ts
+++ app/world/world.view.ts
@@ -12,16 +12,16 @@ namespace $.$$ {
 			return this.banks_data().find( ( bank ) => bank.id == id )
 		}
 
-		center(next? : $mol_vector_2d<number> | readonly[number,number] , force? : $mol_mem_force ): $mol_vector_2d<number> | readonly[number,number] {
+		center(): $mol_vector_2d<number> {
 			const bank_id = this.$.$mol_state_arg.value('bank' )
 			console.log(bank_id)
 			if (bank_id) {
 				const central_bank = this.bank_id(bank_id)
 				console.log('central_bank', central_bank)
-				return [central_bank?.latitude || 0, central_bank?.longitude || 0]
+				return new $mol_vector_2d(central_bank?.latitude || 0, central_bank?.longitude || 0)
 			}
 
-			return [55.754742, 37.621407]
+			return new $mol_vector_2d(55.754742, 37.621407)
 		}
 
 		banks_mark_list() {
@@ -29,7 +29,7 @@ namespace $.$$ {
 			return this.banks_data().map( bank => this.Place(bank.id))
 		}
 
-		mark_pos( id?: string, next?: $mol_vector_2d<number> | readonly[number,number] ): $mol_vector_2d<number> | readonly[number,number] {
+		mark_pos( id: any ): $mol_vector_2d<number> {
 			const bank = this.bank_id( id || '' )
 			return new $mol_vector_2d( bank?.latitude || 0, bank?.longitude || 0 )
 		}
