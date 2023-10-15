b521108ad6c5a06779a12899a351e395e8a2e81f
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-10-15 04:26:44 +0300

    result

--- app/calc/calc.view.ts
+++ app/calc/calc.view.ts
@@ -1,7 +1,14 @@
 namespace $.$$ {
 	export class $origami_app_calc extends $.$origami_app_calc {
 		
-		
+		fetch_loan() {
+			const uri = `https://origami-team.site/calc/?loan_amount=${this.amount()}&interest_rate=${this.rate()}&loan_term=${this.term()}`
+			return this.$.$mol_fetch.json( uri )
+		}
+		loan_submit( next?: any ) {
+			console.log( next )
+			this.fetch_loan()
+		}
 		
 	}
 }
